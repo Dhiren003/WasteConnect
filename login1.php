c2a7056dfdf71af6242ef45bed31a65ebf2fa5be
Meet Patel <meetcpatel906@gmail.com>
2019-02-02 21:46:59 +0530

    table changed

--- login1.php
+++ login1.php
@@ -33,7 +33,7 @@ if (isset($_GET['action']))
     if ($_GET['action'] == 'add')  
         {  
         /*Insert data.*/  
-        $insertSql = "INSERT INTO empTable1 (email,First_name,Last_name,Mobile_Number,uaddress,upassword)   
+        $insertSql = "INSERT INTO empTable (email,First_name,Last_name,Mobile_Number,uaddress,upassword)   
 VALUES (?,?,?,?,?,?)";  
         $params = array( &$_POST['t_email'],&$_POST['t_f_name'], &$_POST['t_l_name'],&$_POST['t_con_no'],&$_POST['t_add'],&$_POST['t_pass']
             );  
