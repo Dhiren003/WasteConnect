76bed30aadf0cc9f7d098a0ecd17dc0d91bd5877
Meet Patel <meetcpatel906@gmail.com>
2019-02-02 20:58:05 +0530

    a

--- sql.php
+++ sql.php
@@ -5,47 +5,14 @@
 <body>  
 <form method="post" action="?action=add" enctype="multipart/form-data" >
 
-<div class="sign-up-htm">
-                <div class="group">
-                    <label for="email_id" class="label1">Email Id</label>
-                    <input id="email_id" type="email" class="input" name="t_email">
-                </div>
-                <div class="group">
-                    <label for="fname" class="label1">First Name</label>
-                    <input id="fname" type="text" class="input" name="t_f_name">
-                </div>
-                <div class="group">
-                    <label for="lname" class="label1">Last Name</label>
-                    <input id="lname" type="text" class="input" name="t_l_name">
-                </div>
-                <div class="group">
-                    <label for="cno" class="label1">Contact Number</label>
-                    <input id="cno" type="number" class="input" name="t_con_no">
-                </div>
-                <div class="group">
-                    <label for="add" class="label1">Address</label>
-                    <textarea id="add" class="input" name="t_add"></textarea>
-                </div>
-                <div class="group">
-                    <label for="pass" class="label1">Password</label>
-                    <input id="pass" type="password" class="input" data-type="password" name="t_pass">
-                </div>
-                <div class="group">
-                    <input type="submit" class="button" value="Sign Up">
-                </div>
-                <div class="hr"></div>
-                <div class="foot-lnk">
-                    <label for="tab-1">Already Member?</label>
-                </div>
-            </div>
 
-<!-- First Name <input type="text" name="t_fname" id="t_fname"/></br>  
+First Name <input type="text" name="t_fname" id="t_fname"/></br>  
 Last Name <input type="text" name="t_lname" id="t_lname"/></br>  
 Mobile Number <input type="text" name="t_Mobile_number" id="t_Mobile_number"/></br>  
 E-mail address <input type="text" name="t_email" id="t_email"/></br>
 Residence Address <input type="text" name="t_uaddress" id= "t_uaddress"> 
 Password <input type="text" name="t_upassword" id= "t_upassword"> 
-<input type="submit" name="submit" value="Submit" />  -->
+<input type="submit" name="submit" value="Submit" />  
 </form>  
 <?php  
 /*Connect using SQL Server authentication.*/  
