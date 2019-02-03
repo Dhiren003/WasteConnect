e5caac03c84bf5ae2b6c9e45abc40f402181c4e3
Dhiren003 <37339387+Dhiren003@users.noreply.github.com>
2019-02-03 08:05:16 +0530

    Update order.php

--- order.php
+++ order.php
@@ -77,7 +77,7 @@
                                     <input id="tab-1" type="radio" name="tab" class="sign-in" checked><label for="tab-1" class="tab">Order Details</label>
                                     <input id="tab-2" type="radio" name="tab" class="sign-up"><label for="tab-2" class="tab"></label>
                                     <div class="login-form">
-                                        <form method="post" action="?action=add" enctype="multipart/form-data">
+                                        <form method="post" action="order-summary.php" enctype="multipart/form-data">
                                         <div class="sign-in-htm">
                                             <div class="group">
                                                <br> <label for="item" class="label1">Item</label>
@@ -137,7 +137,7 @@ if (isset($_GET['submit']))
         /*Insert data.*/  
         $insertSql = "INSERT INTO order1 (Item,Quantity,Estimated_Price)   
 VALUES (?,?,?)";  
-        $params = array( &$_POST['item'],&$_POST['quanty'], &$_POST['price']);  
+        $params = array( &$_POST['item'], &$_POST['quanty'], &$_POST['price']);  
         $stmt = sqlsrv_query($conn, $insertSql, $params);  
         if ($stmt === false)  
             {  
